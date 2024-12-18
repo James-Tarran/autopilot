d87cdfa29f4137dcefee7f2e5c87165823cd8608
James Tarran <109956898+James-Tarran@users.noreply.github.com>
2024-12-18 11:02:09 +0000

    Update autopilot.ps1

--- autopilot.ps1
+++ autopilot.ps1
@@ -1,18 +1,38 @@
 $ProgressPreference = 'SilentlyContinue'
+function CountDown() {
+    param($timeSpan)
+
+    $spinner = @('|', '/', '-', '\')
+    $colours = @("Red", "DarkRed", "Magenta", "DarkMagenta", "Blue", "DarkBlue", "Cyan", "DarkCyan", "Green", "DarkGreen", "Yellow", "DarkYellow", "White", "Gray", "DarkGray", "Black")
+    $colourIndex = 0
+
+    while ($timeSpan -gt 0)
+        {
+            foreach ($spin in $spinner) {
+                Write-Host "`r$spin" -NoNewline -ForegroundColor $colours[$colourIndex]
+                Start-Sleep -Milliseconds 90
+            }
+            $colourIndex++
+            if ($colourIndex -ge $colours.Length) {
+                $colourIndex = 0
+            }
+            $timeSpan = $timeSpan - 1
+        }
+}
 # Configure 
-Set-NetConnectionProfile -NetworkCategory Private
-winrm quickconfig -quiet
+Set-NetConnectionProfile -NetworkCategory Private | out-null
+winrm quickconfig -quiet | out-null
 
 # Get tenant ID
-Install-PackageProvider NuGet -force -ErrorAction SilentlyContinue
-Import-PackageProvider -Name NuGet -ErrorAction SilentlyContinue
-install-module Microsoft.Graph.Authentication -Force -scope CurrentUser -ErrorAction SilentlyContinue -Confirm:$false
-install-module Microsoft.Graph.Identity.DirectoryManagement -force -scope CurrentUser -ErrorAction SilentlyContinue -Confirm:$false
-import-module Microsoft.Graph.Authentication -ErrorAction SilentlyContinue -force
-import-module Microsoft.Graph.Identity.DirectoryManagement -ErrorAction SilentlyContinue -force
+Install-PackageProvider NuGet -force -ErrorAction SilentlyContinue | out-null
+Import-PackageProvider -Name NuGet -ErrorAction SilentlyContinue | out-null
+install-module Microsoft.Graph.Authentication -Force -scope CurrentUser -ErrorAction SilentlyContinue -Confirm:$false | out-null
+install-module Microsoft.Graph.Identity.DirectoryManagement -force -scope CurrentUser -ErrorAction SilentlyContinue -Confirm:$false | out-null
+import-module Microsoft.Graph.Authentication -ErrorAction SilentlyContinue -force | out-null
+import-module Microsoft.Graph.Identity.DirectoryManagement -ErrorAction SilentlyContinue -force | out-null
 Connect-MgGraph -nowelcome
 $TenantID = (Get-MgOrganization).id
-disconnect-mggraph
+disconnect-mggraph | out-null
 
 # Initialise
 $bad = $false
@@ -47,12 +67,14 @@ $c = New-Object psobject -Property @{
 
 # Write the object to the pipeline or array
 if ($bad) {
-    # Report an error when the hash isn't available
+    # Report an error when the hash isn't availacable
     Write-Error -Message "Unable to retrieve device hardware data (hash) from computer" -Category DeviceError
 }
 
 $computers += $c
-Write-Host "Gathered details for device with serial number: $serial"
+Write-Host "Gathered details for device; please allow 60 minutes for processing, then restart your device.`nIf you are still not prompted to sign in with your corporate account after 60 minutes, please contact technicalsupport@techary.com"
+countdown 3600
+Write-host "Please now restart your computer"
 $computers
 
 Remove-CimSession $session
